c6d232f3fc336c7383ab42c779de4b47d2a0dc11
Lei <koudash@gmail.com>
2019-06-17 14:29:28 -0500

    final version

--- static/js/app.js
+++ static/js/app.js
@@ -9,19 +9,20 @@ Promise.all([
   d3.json(tectonicPlatesUrl)
 ]).then((data) => {
   
-  // Create GeoJSON layer for tectonic plate layer 
+  // .......... OVERLAY LAYERS .......... //
+  // Create GeoJSON LAYER FOR TECTONIC PLATES 
   let tectonicBndr = L.geoJson(data[1], {
     color: "orange",
     weight: 2
   });
 
-  // Variable for features array
+  // Variable for features array from earthquake data
   let features = data[0].features;
 
-  // Create GeoJSON layers
+  // Create GeoJSON LAYERS FOR EARTHQUAKES
   let earthquakes = L.geoJSON(features, {
 
-    // Cricles determined by "mag"
+    // 1. Cricles determined by "mag"
     pointToLayer: (feature, latlng) => {
       return new L.circle(latlng, {
         radius: radiusPuller(feature),
@@ -30,11 +31,10 @@ Promise.all([
         stroke: true,
         weight: 0.5
       });
-      console.log(radius);
       
     },
     
-    // Event listener for "mouseover" and "mouseout"
+    // 2. Event listener for "mouseover" and "mouseout"
     onEachFeature: (feature, layer) => {
 
       layer.on({
@@ -60,8 +60,16 @@ Promise.all([
 
     }
 
+  // end of earthquake layers creation
   });
 
+  // Create OVERLAY OBJECT to hold overlay layers
+  let overlayMaps = {
+    "Fault Lines": tectonicBndr,
+    "Earthquakes": earthquakes
+  }; 
+
+    // .......... BASE MAP LAYERS .......... //
   // Define map layers
   let streetsSatelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -81,34 +89,28 @@ Promise.all([
         id: "mapbox.outdoors",
         accessToken: API_KEY
       });
-
   
-  // Define "baseMaps" object to hold base layers
+  // Create BASE MAP OBJECT to hold base map layers
   let baseMaps = {
     "Satellite": streetsSatelliteMap,
     "Grayscale": lightMap,
     "Outdoors": outdoorMap
   };
 
-  // Create overlay object to hold overlay layers
-  let overlayMaps = {
-    "Fault Lines": tectonicBndr,
-    "Earthquakes": earthquakes
-  }; 
-  
-  // Leaflet default map
+  // .......... MAP .......... //
+  // Create MAP OBJECT and set default layers
   let myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 3,
     layers: [streetsSatelliteMap, tectonicBndr, earthquakes]
   });  
 
-  // Create a layer control and add it to the map
+  // Create a layer control for base map and overlay layers and add it to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
-  // Create legend on bottom right of the chart
+  // Create legend on bottom right of the map
   // https://gis.stackexchange.com/questions/133630/adding-leaflet-legend
   let legend = L.control({
     position: "bottomright"
@@ -119,25 +121,26 @@ Promise.all([
     let div = L.DomUtil.create('div', "legend"),
         dummyMagArr = [0, 1, 2, 3, 4, 5];
     
-    for (let i = 0; i < dummyMagArr.length; i++) {
-      
+    for (let i = 0; i < dummyMagArr.length; i++) {      
       div.innerHTML +=
         "<i style=\"background-color: " + colorPuller(dummyMagArr[i] + 1) + "\"></i> " +
         dummyMagArr[i] + (dummyMagArr[i + 1] ? "-" + dummyMagArr[i + 1] + "<br>" : "+");
-
     }
 
     return div;
+
   };
 
+  // Add legend to the map
   legend.addTo(myMap);
 
 
-
+  // |||||||||| FUNCTIONS |||||||||| //
   /**
    * Determine the visualization color for each datapoint based on its magnitude point value
    * @param {*} feature feature of each datapoint retrieved from earthquake url
    */
+  // https://leafletjs.com/examples/choropleth/
   function colorPuller(mag) {
 
     return mag > 5 ? "orangered" :
