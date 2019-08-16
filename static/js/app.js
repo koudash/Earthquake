49cc09c1f2dc4606d8a52fcfb4ba3e9b9ac1621d
Lei <koudash@gmail.com>
2019-08-15 23:53:19 -0500

    change lines for long codes

--- static/js/app.js
+++ static/js/app.js
@@ -72,19 +72,25 @@ Promise.all([
     // .......... BASE MAP LAYERS .......... //
   // Define map layers
   let streetsSatelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    attribution: `Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, 
+      <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © 
+      <a href=\"https://www.mapbox.com/\">Mapbox</a>`,
     maxZoom: 18,
     id: "mapbox.streets-satellite",
     accessToken: API_KEY
   }),
       lightMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        attribution: `Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, 
+          <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © 
+          <a href=\"https://www.mapbox.com/\">Mapbox</a>`,
         maxZoom: 18,
         id: "mapbox.light",
         accessToken: API_KEY
       }),
       outdoorMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        attribution: `Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, 
+          <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © 
+          <a href=\"https://www.mapbox.com/\">Mapbox</a>`,
         maxZoom: 18,
         id: "mapbox.outdoors",
         accessToken: API_KEY
